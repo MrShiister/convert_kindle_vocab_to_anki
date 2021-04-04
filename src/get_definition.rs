0048f10fcb5dfbad03d493b8365d23e436372a06
MrShiister <MrShiister@users.noreply.github.com>
2021-04-04 21:33:40 +0800

    Intermediate save. Cannot figure out how to use FuturesOrdered

--- src/get_definition.rs
+++ src/get_definition.rs
@@ -1,18 +1,24 @@
 use std::error::Error;
-use super::debug_print;
+use super::{debug_print, Word};
 use surf;
 use serde_json::Value;
-use futures::executor::block_on;
+use futures::{
+    executor::block_on,
+    stream::FuturesOrdered,
+};
 
-pub fn get_definition(vocab: &str, v: u8) -> Result<(), Box<dyn Error>> {
+pub fn get_definition(vocablist: Vec<String>, v: u8) -> Result<Vec<Word>, Box<dyn Error>> {
 
-    debug_print(format!("Received String: {}", vocab), 2, v);
+    let urilist: Vec<String> = Vec::new();
+    let wordlist: Vec<Word> = Vec::new();
 
-    let uri = format!("https://dictionaryapi.com/api/v3/references/collegiate/json/{}?key=04a5d981-0869-42c8-a87c-c8cbfdcfcb56", vocab);
+    for vocab in vocablist {
+        debug_print(format!("Received Vocab: {}", vocab), 2, v);
+        let uri = format!("https://dictionaryapi.com/api/v3/references/collegiate/json/{}?key=04a5d981-0869-42c8-a87c-c8cbfdcfcb56", vocab);
+        urilist.push(uri);
+    }
 
-    let res = block_on(get_json(uri));
-
-    let res = match res {
+    let jsonlist = match block_on(get_json(urilist)) {
         Ok(s) => s,
         Err(e) => {
             eprintln!("{}", e);
@@ -20,23 +26,44 @@ pub fn get_definition(vocab: &str, v: u8) -> Result<(), Box<dyn Error>> {
         }
     };
 
-    // clean the json
-    // print the info
-    println!("{}", res);
+    for (i, json) in jsonlist.iter().enumerate() {
+        debug_print(format!("Received json:\n{}", json), 3, v);
 
-    Ok(())
+        let word = Word {
+                        word: vocablist[i].to_string(),
+                    headword: json[0]["hwi"]["hw"].to_string(),
+              pronunciations: json[0]["hwi"]["prs"][0]["mw"].to_string(),
+            example_sentence: "".to_string(),
+                  definition: json[0]["shortdef"].to_string(),
+        };
+        
+        debug_print(format!("{}", word), 1, v);
+        wordlist.push(word);
+    }
+
+    Ok(wordlist)
+        
 }
 
-pub async fn get_json(uri: String) -> Result<Value, Box<dyn Error>> {
+pub async fn get_json(urilist: Vec<String>) -> Result<Vec<Value>, Box<dyn Error>> {
 
-    //#[derive(Deserialize, Serialize)]
-    let res = match surf::get(uri).recv_json().await {
-        Ok(s) => s,
-        Err(e) => {
-            println!("{}", e);
-            return Err("get failed".into())
-        }
-    };
+    let reslist: Vec<Value> = Vec::new();
+    let futlist = FuturesOrdered::new();
 
-    Ok(res)
+    for uri in urilist {
+        let fut = surf::get(uri).recv_json();
+        futlist.push(fut);
+    }
+    
+    futlist.collect::<Vec<_>>(); // TODO: How to poll/collect?
+
+//    let res = match surf::get(uri).recv_json().await {
+//        Ok(s) => s,
+//        Err(e) => {
+//            eprintln!("{}", e);
+//            // return Err("get failed".into())
+//        }
+//    };
+
+    Ok(reslist)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
 mod get_definition;
 pub use crate::get_definition::get_definition;
 use clap::ArgMatches;
-use std::process;
+use std::{
+    fmt,
+    process,
+};
+
+pub struct Word {
+                word: String, // original word
+            headword: String, // 2.3 hw
+      pronunciations: String, // 2.6 prs
+    example_sentence: String, // imported
+          definition: String, // 2.10.2 def
+}
+
+impl fmt::Display for Word {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "Word: {}\nHeadword: {}\nPronunciation: {}\nExample Sentence: {}\nDefinition: {}", self.word, self.headword, self.pronunciations, self.example_sentence, self.definition)
+    }
+}
 
 fn debug_print(message: String, message_verbosity: u8, verbosity: u8) {
     if verbosity >= message_verbosity {
@@ -12,20 +29,29 @@ fn debug_print(message: String, message_verbosity: u8, verbosity: u8) {
 pub fn run(matches: ArgMatches) {
 
     // Get the desired verbosity
-    let v = match matches.occurrences_of("v") {
+    let mut v = match matches.occurrences_of("v") {
         0 => 0,
         1 => 1,
-        2 | _ => 2,
+        2 => 2,
+        3 | _ => 3,
     };
 
     // Check if we are only doing test.
     if let Some(matches) = matches.subcommand_matches("test") {
+
+        let vocablist: Vec<String> = Vec::new();
+
         let vocab = matches.value_of("vocab").unwrap();
-        debug_print(format!("Testing on vocab: {}", vocab), 0, v);
-        if let Err(e) = get_definition(&vocab, v) {
+        vocablist.push(vocab.to_string());
+
+        // Minimum 1 verbosity for test
+        if v == 0 { v = 1 };
+
+        if let Err(e) = get_definition(vocablist, v) {
             eprintln!("Failed to get definition: {}", e);
             process::exit(1);
         }
+
     } else {
     // We are doing the real thing.
         debug_print(format!("Writing results to: {}", matches.value_of("OUTFILE").unwrap()), 1, v);
